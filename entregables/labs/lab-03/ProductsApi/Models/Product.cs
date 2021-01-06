abaf5313a27e15c31e2ed308d47b0e0a8aff8ccc
Cesar Bejarano <cesar@growthaccelerationpartners.com>
2021-01-05 20:49:56 -0600

    Subiendo lab-03

---
+++ entregables/labs/lab-03/ProductsApi/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProductsApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ entregables/labs/lab-03/ProductsApi/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ProductsApi.Models;
+using Core.Data;
+
+namespace Repositories
+{
+    public class ProductRepository
+    {
+        private readonly AdventureWorksDbContext _context;
+
+        public ProductRepository(AdventureWorksDbContext context)
+        {
+            _context = context;
+        }
+
+        public Product[] Get()
+        {
+            return _context.Products.ToArray();
+        }
+
+        public Product Add(Product value)
+        {
+            _context.Products.Add(value);
+
+            _context.SaveChanges();
+
+            return value;
+        }
+
+        public void Delete(int id)
+        {
+            var match = _context.Products.Find(id);
+            if (match != null)
+            {
+                _context.Products.Remove(match);
+            }
+            _context.SaveChanges();
+        }
+    }
+}
